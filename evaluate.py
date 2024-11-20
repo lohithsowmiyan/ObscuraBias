ccf43c50622faa8cff19f642434b7483cfd663b6
Lohith Sowmiyan Panjalingapuram Senthilkumar <lpanjal@vclvm176-249.vcl.ncsu.edu>
2024-11-19 21:15:31 -0500

    import package in evaluation.py

--- evaluate.py
+++ evaluate.py
@@ -1,21 +1,18 @@
 from transformers import pipeline
-from detoxify import Detoxify
+import evaluate 
 
 class Metrics:
     def __init__(self):
-
-        self.regard =  regard = evaluate.load("regard", "compare")
-
-        self.toxicity  =   evaluate.load("toxicity")
-
+        self.regard = evaluate.load("regard", "compare")
+        self.toxicity = evaluate.load("toxicity")
 
     def calc_regard(self, responses, precision = 2):
         regard_results = self.regard.compute(data = profession1_completions, references = profession2_completions)
         return round(regard_results['regard_difference']['negative'], 2)
 
-        
-
     def calc_toxicity(self, responses):
         results = self.toxicity.compute(predictions=male_model_completions, aggregation="ratio")
         return results
+
+    
 
