29c08b99dd2c3723cc3fa48d119a7ba2879e5bbf
lohith sowmiyan <lohithsowmiyan@lohiths-MacBook-Pro.local>
2024-12-01 12:43:06 -0500

    added all the meta features

--- bias.py
+++ bias.py
@@ -22,33 +22,48 @@ class BiasWrapper():
         Methods:
             1. get_data_wrapper: is the member function to perform the infernce for the user sepecified configurations
     """
-    def __init__(self, dataset_path : str, llm_model : str, metrics_list : list, save : bool | str,  args : dict,   **kwargs):
+    def __init__(self, dataset_path : str, llm_model : str, metrics_list : list, cache : bool | str,  args : dict,   **kwargs):
         dataset_object = Dataset()
         dataset_object.load_data(path = args.dataset)
         self.prompts = dataset_object.get_prompts()
         self.distinctions = dataset_object.distinctions
-        self.save = save
+        self.cache = cache
         
        
         args.llm_path = model_path[llm_model]
         self.model = LLM(args)
         self.metrics = Metrics()
 
-    def get_data_wrapper(self):
-       
+    def get_data_wrapper(self) -> bool:
+        """
+        Descriptions: member function to perform the infernce for the user sepecified configurations and returns the csv file if cached
 
-        if(type(self.save) == str):
+        Parameters: None
 
-            save = self.save.split("/")[-1]
-            
-            directory_path = f'causal_datasets/{save[:-5]}.csv'
-            print(directory_path)
-            if os.path.exists(directory_path):
-                print(f"File '{directory_path}' already exists. Passing...")
-            else:
-                data = self.model.infer_and_save(self.prompts, self.metrics)
-                data = pd.DataFrame(data, columns = ['Prompt', self.distinctions[0], self.distinctions[1], 'Context', 'Neg_Regard', 'Toxicity'])
-                data.to_csv(directory_path)
+        Returns: (bool) True if data saved successfully otherwise False if a Exeption occurs
+        
+        """
+        
+        try:
+            if(type(self.cache) == str):
+
+                save = self.cache.split("/")[-1]
+                
+                directory_path = f'causal_datasets/{save[:-5]}.csv'
+                print(directory_path)
+                if os.path.exists(directory_path):
+                    print(f"File '{directory_path}' already exists. Passing...")
+                else:
+                    data = self.model.infer_and_save(self.prompts, self.metrics)
+                    data = pd.DataFrame(data, columns = ['Prompt', self.distinctions[0], self.distinctions[1], 'Context', 'Neg_Regard', 'Toxicity'])
+                    data.to_csv(directory_path)
+
+        except Exception:
+            print("File not saved")
+            return False
+
+        finally:
+            return True
                 
 
         
