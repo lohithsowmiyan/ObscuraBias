36c9b768bf7bb024efac3178341610820982ed48
lohith sowmiyan <lohithsowmiyan@lohiths-MacBook-Pro.local>
2024-12-01 12:58:48 -0500

    added docstring for dataset

--- dataset.py
+++ dataset.py
@@ -1,6 +1,18 @@
 import json
 
 class Dataset():
+    """
+    Description : it is the custom dataset object, 
+    loads the data from the json files into the desired format that would make it easier to work with during inferencing with large language models
+
+    Arguments: (None)
+
+    Methods: 
+        1. load_data: is the member function to load the raw data from the json file
+        2. get_promnpts: converts the raw data from the json file into desired format
+        3. get_distributions: a dictionary containing the meta data about the data
+    """
+
     def __init__(self):
         self.name = ""
         self.prompts = {}
@@ -8,17 +20,40 @@ class Dataset():
     def __repr__(self):
         return f"DATASET_{self.name}"
 
-    def load_data(self, path = 'datasets/physical_gender.json'):
+    def load_data(self, path = 'datasets/physical_gender.json') -> bool:
+        """
+        Descriptoion : is the member function to load the raw data from the json file
 
-        with open(path, 'r') as file:
-            dataset = json.load(file)
+        Paramaters:
+            1. path: (str) is the path  to the specified json file containing the dataset
+                Example : path = 'datasets/physical_gender.json'
+
+        Returns: (bool) true if loaded otherwise false
+        """
+        try:
+            with open(path, 'r') as file:
+                dataset = json.load(file)
+        
+        except:
+            print("cannot load from the the dataset")
+            return False
 
         self.name = dataset['type']
         self._attributes = dataset['distinctions']
         self.dataset = dataset['data']
-       # self.contexts = [context for context in self.dataset['context']]
 
-    def get_prompts(self):
+        return True
+      
+
+    def get_prompts(self) -> dict[str: dict[str: list]]:
+        """
+        Description : converts the raw data from the json file into desired format
+
+        Parameters: None
+
+        Returns:  (dict[str: dict[str: list]]) a dictionary containing the data which has key as all the treatmets (interesectional biases),  
+        with values once again being dictionaries with contexts as key and list of prompts as values
+        """
 
         for dobj in self.dataset:
             context = dobj['context']
@@ -45,13 +80,15 @@ class Dataset():
         return self._attributes
 
 
-    def get_distribution(self):
+    def get_distribution(self) -> dict:
+        """ 
+        Description : a dictionary containing the meta data about the data
+
+        Parameters: None
+
+        Returns : a dicitonary with keys as treatmets and number of prompts per treatment as values
+        """
         return {key : len(value) for key, value in self.prompts.items()}
-
-
-    # @property
-    # def contexts(self):
-    #     return self.contexts
 
 
     
